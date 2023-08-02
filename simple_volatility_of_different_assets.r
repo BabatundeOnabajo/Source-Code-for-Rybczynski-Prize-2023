05ec38fb955adf4af12a5b436b0f2f2f30fef2ce
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2023-08-02 08:58:33 +0100

    Update simple_volatility_of_different_assets.r

--- simple_volatility_of_different_assets.r
+++ simple_volatility_of_different_assets.r
@@ -1,2 +1,2 @@
-# This is the R script used to calculate the historical volatility from 2009 for a range of assets.
+# This is the R script used to calculate the historical volatility from 2009 for a range of assets. The variance of a given dataset is given by the var() function.
 
