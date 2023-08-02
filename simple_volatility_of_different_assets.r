e19f1f361961cb74dc0a293428d76fcf636b1d4d
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2023-08-02 23:09:44 +0100

    Update simple_volatility_of_different_assets.r

--- simple_volatility_of_different_assets.r
+++ simple_volatility_of_different_assets.r
@@ -2,3 +2,6 @@
 
 ### If you do not have the package qcc, then it is recommended you uncomment this line so that you can have it in R. qcc enables us to use exponential weighted moving average for our datasets.
 install.packages('qcc')
+
+### Download the close data for the S&P 500.
+
