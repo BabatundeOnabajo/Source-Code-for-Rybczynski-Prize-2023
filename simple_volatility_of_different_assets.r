18b777813d83875c9837d763ba724d6b96fe9220
Babatunde Valentine Onabajo <36359216+BabatundeOnabajo@users.noreply.github.com>
2023-08-02 14:28:56 +0100

    Update simple_volatility_of_different_assets.r

--- simple_volatility_of_different_assets.r
+++ simple_volatility_of_different_assets.r
@@ -1,2 +1,4 @@
 # This is the R script used to calculate the historical volatility from 2009 for a range of assets. The variance of a given dataset is given by the var() function.
 
+### If you do not have the package qcc, then it is recommended you uncomment this line so that you can have it in R. qcc enables us to use exponential weighted moving average for our datasets.
+install.packages('qcc')
